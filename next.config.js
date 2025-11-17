cb761f26fa841d3a5e36ab7efe32e42ab6306a31
akshaypareek01 <akshay96102@gmail.com>
2025-11-17 17:38:20 +0530

    wokring

---
+++ next.config.js
@@ -0,0 +1,15 @@
+
+/**@type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  trailingSlash: true,
+  swcMinify: true,
+  images: {
+    unoptimized: true,
+  },
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+};
+
+module.exports = nextConfig;
